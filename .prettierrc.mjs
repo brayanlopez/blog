bb2ba05d436a5e4ad5578465002a1e02246ed67f
Brayan A. Lopez G. <balg.lg55@gmail.com>
2024-01-08 20:07:32 -0500

    ADD prettier configuration and format project

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	plugins: ['prettier-plugin-astro'],
+	overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
+}
