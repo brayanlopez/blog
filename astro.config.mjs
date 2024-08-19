797a144a8b11f6a544a77a895f770989b5e3537c
brayanLopez <balg.lg55@gmail.com>
2024-08-18 20:51:14 -0500

    CONFIG github pages deploy using github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import vue from '@astrojs/vue'
-
 import svelte from '@astrojs/svelte'
 import sitemap from '@astrojs/sitemap'
+import { PROJECT_BASE } from './src/common/constants.mjs'
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astrojs.com',
+	site: 'https://brayanlopez.github.io',
+	base: PROJECT_BASE,
 	integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()],
 })
